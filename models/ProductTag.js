95b66c3807262d469b4502499a6691ac8df60912
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-05-22 14:57:35 -0700

    fijnsihed the column in productTag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -5,8 +5,28 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
+  { //here we will be getting our data from product tag seeds file 
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id:{
+      type: DataTypes,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
