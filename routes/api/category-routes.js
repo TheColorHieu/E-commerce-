991dafd6a9996afc7c9e315736b3e36e9ae2d985
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-06-07 19:27:29 -0700

    here we made the routes for the category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,16 +31,42 @@ try{
 }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err){
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',async (req, res) => {
   // update a category by its `id` value
+  try{
+  const categoryData = await Category.update(req.body);
+  res.status(200).json(categoryData);
+} catch (err){
+  res.status(404).json(err);
+}
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',async (req, res) => {
   // delete a category by its `id` value
+  try{
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!categoryData){
+      res.status(404).json({message: "There was no product found with this id"});
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
