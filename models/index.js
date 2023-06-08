0107905129b3622be1df343d4399ac345bd8f2a2
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-06-07 19:17:10 -0700

    finished index.js

--- models/index.js
+++ models/index.js
@@ -15,19 +15,15 @@ Category.hasMany(Product, {
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag, 
-    unique: false
-  },
-  as: "product_id",
+  through: ProductTag,
+  foreignKey: 'product_id',
+  otherKey: 'tag_id',
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: "tag_id",
+  through: ProductTag,
+  foreignKey:'tag_id',
+  otherKey:'product_id',
 });
 module.exports = {
   Product,
